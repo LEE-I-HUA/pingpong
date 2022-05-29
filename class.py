d59c03fbe1a53e693fedc4494a2f2ca48c1eeda1
LEE-I-HUA <93990513+LEE-I-HUA@users.noreply.github.com>
2022-05-29 21:46:05 +0800

    Update class.py

--- class.py
+++ class.py
@@ -1,6 +1,11 @@
 import turtle as t
 import random
 import time
+import os
+
+t.bgcolor("lightgreen")
+t.setup(500, 700)
+
 
 class Player:
     def __init__(self, initX, initY):
@@ -11,123 +16,148 @@ class Player:
         self.turtle.speed(0)
         self.turtle.goto(initX,initY)
         self.score = 0
+        # self.turtle.listen()
+
+    def setColor(self, color):
+        self.turtle.color(color)
 
     def right(self):
         if self.turtle.xcor()<200:
-            self.turtle.forward(50)
+            self.turtle.forward(60)
     
     def left(self):
         if self.turtle.xcor()>-200:
-            self.turtle.backword(50)
+            self.turtle.backward(60)
+
 
     def distance(self, ball) -> float : 
-        return self.turtle.distance(ball)
+         return self.turtle.distance(ball)
 
-    def hit(lowerBound, upperBound):
-        if Player.distance(ball) < 50 and lowerBound< ball.ycor() < upperBound:
-            ball_yspeed *= -1
-
+    # def getKey(self,rightKey, leftKey):
+    #     self.turtle.listen()
+    #     self.turtle.onkeypress(self.right, rightKey)
+    #     self.turtle.onkeypress(self.left, leftKey)  
 
 class Ball:
-    def __init__(self) -> None:
+    def __init__(self, difficulty) -> None:
         self.turtle = t.Turtle()
         self.turtle.shape("circle")
         self.turtle.shapesize(1.3)
         self.turtle.speed(0)
         self.turtle.color("white")
-        self.xspeed = 5
-        self.yspeed = 5
+        self.xspeed = difficulty
+        self.yspeed = difficulty
     
     def moveOn(self):
         curX, curY = self.turtle.position()
         self.turtle.goto(curX + ball_xspeed, curY + ball_yspeed)
 
-    def slowDown(self):
-        
+playerA = Player(0, -270)
+playerA.setColor("darkred")
+playerB = Player(0, 270)
+playerB.setColor("darkblue")
 
 
+#难度选择
+difficulty = t.numinput('输入难度：', '可输入1-10级', 3, 1, 10)
+ball_xspeed = difficulty
+ball_yspeed = difficulty
 
-    #def xcor
-# new_x = ball.xcor() + ball_xspeed
-# new_y = ball.ycor() + ball_yspeed
-# ball.goto(new_x, new_y)
-        
-# ball_xspeed = 5
-# ball_yspeed = 5
-# initialize
-t.bgcolor("lightpink")
-t.setup(500, 700)
-pA = Player(0, 270)
-pB = Player(0, -270)
-b = Ball()
+# ball = Ball(difficulty)
 
-#Ball
-#  ball = t.Turtle()
-# ball.shape("circle")
-# ball.shapesize(1.3)
-# ball.up()
-# ball.speed(0)ball
-# ball.color("white")
+def promt_menu():
+    txt = t.textinput('what do you want to do?','you can type [restart/quit/score]:')
+    if txt=="quit":
+        t.Screen().bye()
+    if txt=="restart":
+        t.Screen().bye()
+        os.system('double.py')
+    if txt=="score":
+        t.goto(0,0)
+        t.write(f"Ascore : {playerA.score},Bscore : {playerB.score}", False, "center", ("", 15))
+        time.sleep(3)
+        t.undo()
+
+
+    
+t.onkeypress(promt_menu,"Up")
 
 t.listen()
-t.onkeypress(pA.right,"Right")
-t.onkeypress(pA.left,"Left")
-t.onkeypress(pB.right,"D")
-t.onkeypress(pB.right,"d")
-t.onkeypress(pB.left,"A")
-t.onkeypress(pB.left,"a")
+t.onkeypress(playerA.right,"Right")
+t.onkeypress(playerA.left,"Left")
+t.onkeypress(playerB.right,"D")
+t.onkeypress(playerB.right,"d")
+t.onkeypress(playerB.left,"A")
+t.onkeypress(playerB.left,"a")
 
+Ball
+ball = t.Turtle()
+ball.shape("circle")
+ball.shapesize(1.3)
+ball.up()
+ball.speed(0)
+ball.color("white")
 
+# ball = Ball
 
 game_on = True
-
 
 #life score
 t.up()
 t.ht()
 t.goto(0,300)
-t.write(f"Ascore : {pA.score},Bscore : {pB.score}", False, "center", ("", 15))
+t.write(f"Ascore : {playerA.score},Bscore : {playerB.score}", False, "center", ("", 15))
 
 
 while game_on:
-    b.moveOn()
+    new_x = ball.xcor() + ball_xspeed
+    new_y = ball.ycor() + ball_yspeed
+    ball.goto(new_x, new_y)
 
     if ball.xcor() > 240 or ball.xcor() < -240:
         ball_xspeed *= -1
 
     if ball.ycor() > 340:
-        pB.score += 1
+        playerA.score = playerA.score+1
         t.clear()
         time.sleep(0.5)
-        ball.goto(0, 100)
-        t.write(f"Ascore : {pA.score}, Bscore : {pB.score}", False, "center", ("", 15))
-        ball_yspeed *= -1
-        ball_xspeed *= -1
+        ball.goto(0, -270) 
+        t.write(f"Ascore : {playerA.score},Bscore : {playerB.score}", False, "center", ("", 15))
+        ball_yspeed = abs(ball_yspeed)
+        ball_xspeed = abs(ball_xspeed)
+
+        if playerA.score==3:
+           ball.goto(0, 100)  
+           game_on=False
+           t.clear()
+           t.goto(0, 50)
+           t.write(f"Ascore : {playerA.score},Bscore : {playerB.score}", False, "center", ("", 15))
+           t.goto(0,0)
+           t.write("Game Over! player A is the winner!",False, "center",("",20))
 
     if ball.ycor() < -340:
-        pA.score += 1
+        playerB.score = playerB.score+1
         t.clear()
         time.sleep(0.5)
-        ball.goto(0, 100)
-        t.write(f"Ascore : {pA.score},Bscore : {pB.score}", False, "center", ("", 15))
-        ball_yspeed *= -1
-        ball_xspeed *= -1
+        ball.goto(0, 270)
+        t.write(f"Ascore : {playerA.score},Bscore : {playerB.score}", False, "center", ("", 15))
+        ball_yspeed = -abs(ball_yspeed)
+        ball_xspeed = -abs(ball_xspeed)
 
-    if pA.score==3 or pB.score==3:
-        game_on=False
-        t.goto(0, 0)
-        t.write("Game Over", False, "center", ("", 20))
+        if playerB.score==3:
+            ball.goto(0, 100)  
+            game_on=False
+            t.clear()
+            t.goto(0, 50)
+            t.write(f"Ascore : {playerA.score},Bscore : {playerB.score}", False, "center", ("", 15))
+            t.goto(0, 0)
+            t.write("Game Over! playerB is the winner!", False, "center", ("", 20))
 
-    pA.hit(-260, -245)
 
-    pB.hit(245, 260)
-
-
-    # if pA.distance(ball) < 50 and -260 < ball.ycor() < -245:
-    #     ball_yspeed *= -1
+    if playerA.distance(ball) < 50 and (-260 < ball.ycor() and ball.ycor() < -245):
+        ball_yspeed = abs(ball_yspeed)
         
-    # if pB.distance(ball) < 50 and 245 < ball.ycor() < 260:
-    #     ball_yspeed *= -1
-
+    if playerB.distance(ball) < 50 and (245 < ball.ycor() and ball.ycor()  < 260):
+        ball_yspeed = -abs(ball_yspeed)
 
 t.done()
